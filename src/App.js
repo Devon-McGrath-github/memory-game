744317d17268d8aff7e2c7ccbcea350508f2d59b
Devon McGrath <devon.mcgrath@gmail.com>
2023-07-12 09:13:15 +1200

    Layout application, create basic component structure and initial card logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory Card game</h1>
+      <h3>Get points by clicking on an image. Just don't click on the same image twice!</h3>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Card() {
+  return (
+    <div>
+      <div>IMAGE</div>
+      <div>Name</div>
+    </div>
+  );
+}
