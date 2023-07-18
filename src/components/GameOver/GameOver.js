9a30a19998e083eb0cb0965948f5aa90a35b246b
Devon McGrath <devon.mcgrath@gmail.com>
2023-07-19 11:16:55 +1200

    Rearrange GameOver components to be under one folder

--- src/components/GameOver/GameOver.js
+++ src/components/GameOver/GameOver.js
@@ -1,21 +1,16 @@
 import React, { useContext } from 'react';
 import './GameOver.css';
 import Replay from '../Replay/Replay';
-import Win from '../Win/Win';
+import Win from './Win/Win';
+import Loss from './Loss/Loss';
 import GameContext from '../../context/GameContext';
 
 export default function GameOver() {
   const { isGameWon } = useContext(GameContext);
   return (
     <div>
-      {isGameWon === true ? (
-        <Win />
-      ) : (
-        <>
-          <h2>You lost, better luck next time!</h2>
-          <Replay />
-        </>
-      )}
+      {isGameWon === true ? <Win /> : <Loss />}
+      <Replay />
     </div>
   );
 }
--- src/components/GameOver/Win/Win.js
+++ src/components/GameOver/Win/Win.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Win.css';
-import Replay from '../Replay/Replay';
 
 export default function Win() {
   return (
     <div>
       <h2>Congratulations, You Won!</h2>
-      <Replay />
     </div>
   );
 }
